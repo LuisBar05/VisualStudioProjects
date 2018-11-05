351a581ad895dafd71694ec5fac9403241c52d43
Luis Bárcenas <luisbar_28@outlook.com>
2018-11-04 20:02:17 -0600

    Botanas!

---
+++ MyFirstApplication/MyFirstApplication/Controllers/HelloWorldController.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Encodings.Web;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyFirstApplication.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /HelloWorld/Welcome/
+        // Requires using System.Text.Encodings.Web;
+
+        public IActionResult Welcome(string name, int numTimes=1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
---
+++ MyFirstApplication/MyFirstApplication/Data/MyFirstApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyFirstApplication.Models
+{
+    public class MyFirstApplicationContext : DbContext
+    {
+        public MyFirstApplicationContext (DbContextOptions<MyFirstApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MyFirstApplication.Models.Movie> Movie { get; set; }
+    }
+}
